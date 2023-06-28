cafd72662cab1eaa47103d73a6b97c6376e79a0b
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-28 22:10:37 +0900

    ✨ [feat] UIView+Extension 추가

---
+++ Cider/Cider/Common/Extension/UIView+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIView.swift
+//  Cider
+//
+//  Created by 임영선 on 2023/06/28.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        views.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(view)
+        }
+    }
+    
+}
