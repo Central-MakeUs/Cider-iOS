42f5dca408d64f357d1690b1cecfaa64f350308f
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-30 15:46:11 +0900

    ✨ [feat] 서비스 동의 뷰에서 접기, 자세히보기 바뀌게 구현

--- Cider/Cider/Presentation/Auth/Views/ServiceAgreeViewController.swift
+++ Cider/Cider/Presentation/Auth/Views/ServiceAgreeViewController.swift
@@ -155,6 +155,7 @@ extension ServiceAgreeViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if (indexPath.section == 1 || indexPath.section == 2) && indexPath.row == 0 {
+            cellData[indexPath.section].subTitle = cellData[indexPath.section].subTitle == "접기" ? "자세히 보기" : "접기"
             cellData[indexPath.section].opened.toggle()
             tableView.reloadData()
         }
