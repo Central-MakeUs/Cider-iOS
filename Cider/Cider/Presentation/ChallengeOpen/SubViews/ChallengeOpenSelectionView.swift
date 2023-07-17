952dea65ba2f0a27e96b5f956e61c52c5d944ee3
youngsun <dudtjs1021@hansung.ac.kr>
2023-07-17 20:01:01 +0900

    ✨ [feat] 챌린지 개설 정원, 기간 pickerView로 선택하게 구현

--- Cider/Cider/Presentation/ChallengeOpen/SubViews/ChallengeOpenSelectionView.swift
+++ Cider/Cider/Presentation/ChallengeOpen/SubViews/ChallengeOpenSelectionView.swift
@@ -22,18 +22,9 @@ final class ChallengeOpenSelectionView: UIView {
         return label
     }()
     
-    private lazy var unitLabel: UILabel = {
-        let label = UILabel()
-        label.text = type.unit
-        label.font = CustomFont.PretendardRegular(size: .base).font
-        label.textColor = .custom.text
-        return label
-    }()
-    
-    private lazy var borderView: UIView = {
+    private lazy var paddingView = {
         let view = UIView()
-        view.layer.borderColor = UIColor.custom.gray2?.cgColor
-        view.layer.borderWidth = 1
+        view.isUserInteractionEnabled = false
         return view
     }()
     
@@ -45,34 +36,109 @@ final class ChallengeOpenSelectionView: UIView {
         return imageView
     }()
     
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.text = String(type.defaultUnit) + type.unitString
+        textField.font = CustomFont.PretendardRegular(size: .base).font
+        textField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        textField.addLeftPadding(12)
+        textField.layer.cornerRadius = 4
+        textField.layer.borderWidth = 0.5
+        textField.layer.borderColor = UIColor.custom.gray2?.cgColor
+        textField.textColor = .custom.text
+        textField.tintColor = .clear
+        textField.inputView = customPickerView
+        textField.isUserInteractionEnabled = true
+        return textField
+    }()
+    
+    private lazy var customPickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        return pickerView
+    }()
+    
+    private lazy var selectedPickerViewData = String(type.defaultUnit) + type.unitString
+    
     init(type: ChallengeOpenSelectionType) {
         self.type = type
         super.init(frame: .zero)
-        configure()
+        setUp()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configure() {
-        addSubviews(mainTitleLabel, subTitleLabel, borderView)
-        borderView.addSubviews(unitLabel, iconImageView)
+}
+
+private extension ChallengeOpenSelectionView {
+    
+    func setUp() {
+        configure()
+        setPickerToolbar()
+    }
+    
+    func configure() {
+        addSubviews(mainTitleLabel, subTitleLabel, textField, paddingView)
+        paddingView.addSubviews(iconImageView)
+        
         NSLayoutConstraint.activate([
             self.heightAnchor.constraint(equalToConstant: 77),
             mainTitleLabel.topAnchor.constraint(equalTo: topAnchor),
             mainTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             subTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             subTitleLabel.topAnchor.constraint(equalTo: topAnchor),
-            borderView.topAnchor.constraint(equalTo: mainTitleLabel.bottomAnchor, constant: 8),
-            borderView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            borderView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            borderView.heightAnchor.constraint(equalToConstant: 44),
-            unitLabel.centerYAnchor.constraint(equalTo: borderView.centerYAnchor),
-            unitLabel.leadingAnchor.constraint(equalTo: borderView.leadingAnchor, constant: 12),
-            iconImageView.centerYAnchor.constraint(equalTo: borderView.centerYAnchor),
-            iconImageView.trailingAnchor.constraint(equalTo: borderView.trailingAnchor, constant: -12)
+            textField.topAnchor.constraint(equalTo: mainTitleLabel.bottomAnchor, constant: 8),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor),
+            paddingView.widthAnchor.constraint(equalToConstant: 12+24),
+            paddingView.heightAnchor.constraint(equalToConstant: 24)
         ])
+        textField.rightView = paddingView
+        textField.rightViewMode = .always
+    }
+    
+    func setPickerToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.barTintColor = .white
+        toolbar.sizeToFit()
+        let cancelButton =  UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+        let doneButton =  UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([cancelButton, space, doneButton], animated: true)
+        textField.inputAccessoryView = toolbar
+    }
+    
+    @objc func didTapDone(_ sender: UIBarButtonItem) {
+        textField.resignFirstResponder()
+        textField.text = selectedPickerViewData
+    }
+    
+    @objc func didTapCancel(_ sender: UIBarButtonItem) {
+        textField.resignFirstResponder()
+    }
+    
+}
+
+extension ChallengeOpenSelectionView: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return type.unitList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(type.unitList[row]) + type.unitString
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedPickerViewData = String(type.unitList[row]) + type.unitString
     }
     
 }
