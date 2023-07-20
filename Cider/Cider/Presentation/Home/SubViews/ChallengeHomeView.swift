c1aec00e68bfc04b2ab586f6a2c35ff16ca567fb
youngsun <dudtjs1021@hansung.ac.kr>
2023-07-20 16:31:15 +0900

    💄 [style] ChallengeHomeView 구현

---
+++ Cider/Cider/Presentation/Home/SubViews/ChallengeHomeView.swift
@@ -0,0 +1,143 @@
+//
+//  ChallengeHomeView.swift
+//  Cider
+//
+//  Created by 임영선 on 2023/07/20.
+//
+
+import UIKit
+
+final class ChallengeHomeView: UIView {
+    
+    private let type: ChallengeType
+    
+    private lazy var statusView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .custom.gray6
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "모집중"
+        label.font = CustomFont.PretendardRegular(size: .sm).font
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var rewardLabel: ChallengeTagLabel = {
+        let label = ChallengeTagLabel(title: "리워드")
+        return label
+    }()
+    
+    private lazy var typeLabel: ChallengeTagLabel = {
+        let label = ChallengeTagLabel(title: type.getKoreanName())
+        return label
+    }()
+    
+    private lazy var dateLabel: ChallengeTagLabel = {
+        let label = ChallengeTagLabel(title: "1주")
+        return label
+    }()
+    
+    private lazy var heartButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "line_like_24"), for: .normal)
+        return button
+    }()
+    
+    init(type: ChallengeType) {
+        self.type = type
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .blue
+        addSubviews(statusView, rewardLabel, typeLabel, dateLabel, statusLabel, heartButton)
+        NSLayoutConstraint.activate([
+            statusView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            statusView.topAnchor.constraint(equalTo: topAnchor, constant: 11.5),
+            statusView.widthAnchor.constraint(equalToConstant: 53),
+            statusView.heightAnchor.constraint(equalToConstant: 21),
+            statusLabel.centerXAnchor.constraint(equalTo: statusView.centerXAnchor),
+            statusLabel.centerYAnchor.constraint(equalTo: statusView.centerYAnchor),
+            heartButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            heartButton.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            typeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            typeLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            dateLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            dateLabel.leadingAnchor.constraint(equalTo: typeLabel.trailingAnchor, constant: 4),
+            rewardLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            rewardLabel.bottomAnchor.constraint(equalTo: typeLabel.topAnchor, constant: -6)
+        ])
+    }
+    
+}
+
+final class ChallengeTagLabel: UILabel {
+    
+    private let title: String
+    private var padding = UIEdgeInsets(top: 2.0, left: 6.0, bottom: 2.0, right: 6.0)
+    
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        configure()
+        setCornerRadius()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+        
+        return contentSize
+    }
+    
+    private func configure() {
+        text = title
+        textAlignment = .center
+        font = CustomFont.PretendardRegular(size: .sm).font
+        textColor = .custom.gray8
+        backgroundColor = .white
+        layer.borderColor = UIColor.custom.gray8?.cgColor
+        layer.borderWidth = 0.5
+    }
+    
+    private func setCornerRadius() {
+        let width = super.intrinsicContentSize.width
+        let height = super.intrinsicContentSize.height
+        let longLength = width > height ? width : height
+        let shortLength = width < height ? width : height
+        let cornerRadius = (longLength * (shortLength/longLength))/2 * 1.2
+        layer.cornerRadius = cornerRadius
+    }
+    
+}
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct ChallengeHomeView_Preview: PreviewProvider {
+    static var previews: some View {
+        
+        UIViewPreview {
+            let view = ChallengeHomeView(type: .financialTech)
+            return view
+        }
+        .previewLayout(.fixed(width: 150, height: 192))
+        .padding(100)
+    }
+}
+#endif
