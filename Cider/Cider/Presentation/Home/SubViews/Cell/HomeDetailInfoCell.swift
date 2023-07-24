f9d09358201cf39a571a44a81ee211fa0923c6ed
youngsun <dudtjs1021@hansung.ac.kr>
2023-07-24 20:16:10 +0900

    🚚 [style] Home/Views/SubViews/Cell,HeaderView폴더 생성

---
+++ Cider/Cider/Presentation/Home/SubViews/Cell/HomeDetailInfoCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeDetailInfoCell.swift
+//  Cider
+//
+//  Created by 임영선 on 2023/07/24.
+//
+
+import UIKit
+
+class HomeDetailInfoCell: UICollectionViewCell {
+    
+}
---
+++ Cider/Cider/Presentation/Home/SubViews/HeaderView/SortingHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  HomeDetailHeaderView.swift
+//  Cider
+//
+//  Created by 임영선 on 2023/07/24.
+//
+
+import UIKit
+
+class SortingHeaderView: UICollectionReusableView {
+        
+}
